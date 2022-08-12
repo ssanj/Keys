058ade682ac758ab34120d90635d4338772dbd3f
sanjiv sahayam <sanjsmailbox@gmail.com>
2022-08-12 11:05:27 +1000

    Add searching for keys

--- Keys.py
+++ Keys.py
@@ -3,7 +3,7 @@ import sublime_plugin
 from typing import Dict, List, Tuple, Any, Optional
 from Keys.Components.KeyInfo import KeyInfo, Command, FileName, Key
 
-class KeysCommand(sublime_plugin.WindowCommand):
+class KeyLogic:
 
   # TODO: Move this to settings
   packages_to_filter_in = \
@@ -26,13 +26,39 @@ class KeysCommand(sublime_plugin.WindowCommand):
 
   @staticmethod
   def is_filtered_package(package: str) -> bool:
-    return len(list(filter(lambda p: package.startswith(p), KeysCommand.packages_to_filter_in))) > 0
+    return len(list(filter(lambda p: package.startswith(p), KeyLogic.packages_to_filter_in))) > 0
+
+
+  @staticmethod
+  def get_key_info() -> List[KeyInfo]:
+    # Should we cache this information?
+    keymaps = sublime.find_resources("*.sublime-keymap")
+    loaded_keymaps_pair: List[Tuple[str, str]] = [(key_map_file, sublime.load_resource(key_map_file)) for key_map_file in keymaps if "(Linux)" not in key_map_file if "(Windows)" not in key_map_file]
+    list_keymap_dict: List[Tuple[str, List[Dict[str, Any]]]] = [(fn, sublime.decode_value(content)) for (fn, content) in loaded_keymaps_pair]
+
+    key_info_list: List[KeyInfo] = []
+    for n, (fn, d) in enumerate(list_keymap_dict):
+      if KeyLogic.is_filtered_package(fn):
+        file_name = FileName(fn)
+        for els in d:
+          possible_keys: List[str] = list(map(lambda k: k.upper(), els['keys']))
+          assert len(possible_keys) > 0, "should have at least one key defined"
+          separate_keys: List[str] = possible_keys[0].split("+") # assume at least one key
+          keys: List[Key] = list(map(lambda k: Key(k.upper()), separate_keys))
+          command = Command(els['command'])
+          key_info_list.append(KeyInfo(file_name, command, keys))
+
+
+    return key_info_list
+
+class KeysCommand(sublime_plugin.WindowCommand):
+
 
   def run(self) -> None:
     window = self.window
 
     if window:
-      key_info_list: List[KeyInfo] = self.get_key_info()
+      key_info_list: List[KeyInfo] = KeyLogic.get_key_info()
 
       key_content = ""
       for key_info in key_info_list:
@@ -51,26 +77,7 @@ class KeysCommand(sublime_plugin.WindowCommand):
     else:
       sublime.message_dialog("No Window found")
 
-  def get_key_info(self) -> List[KeyInfo]:
-    # Should we cache this information?
-    keymaps = sublime.find_resources("*.sublime-keymap")
-    loaded_keymaps_pair: List[Tuple[str, str]] = [(key_map_file, sublime.load_resource(key_map_file)) for key_map_file in keymaps if "(Linux)" not in key_map_file if "(Windows)" not in key_map_file]
-    list_keymap_dict: List[Tuple[str, List[Dict[str, Any]]]] = [(fn, sublime.decode_value(content)) for (fn, content) in loaded_keymaps_pair]
 
-    key_info_list: List[KeyInfo] = []
-    for n, (fn, d) in enumerate(list_keymap_dict):
-      if KeysCommand.is_filtered_package(fn):
-        file_name = FileName(fn)
-        for els in d:
-          possible_keys: List[str] = list(map(lambda k: k.upper(), els['keys']))
-          assert len(possible_keys) > 0, "should have at least one key defined"
-          separate_keys: List[str] = possible_keys[0].split("+") # assume at least one key
-          keys: List[Key] = list(map(lambda k: Key(k.upper()), separate_keys))
-          command = Command(els['command'])
-          key_info_list.append(KeyInfo(file_name, command, keys))
-
-
-    return key_info_list
 
 class ViewKeysCommand(sublime_plugin.TextCommand):
 
@@ -83,3 +90,35 @@ class ViewKeysCommand(sublime_plugin.TextCommand):
       view.set_scratch(True)
     else:
       sublime.message_dialog("No View found")
+
+
+class KeysSearchCommand(sublime_plugin.WindowCommand):
+  def run(self) -> None:
+    window = self.window
+
+    if window:
+      key_info_list: List[KeyInfo] = KeyLogic.get_key_info()
+      panel_items = list(map(lambda ki: self.create_quick_panel_item(window, ki),key_info_list))
+      number_of_items = len(panel_items)
+      if number_of_items > 0:
+        # Don't show previews
+        window.show_quick_panel(
+          items = panel_items,
+          on_select = self.when_key_selected,
+          placeholder = f"Search Keys:"
+        )
+      else:
+        sublime.message_dialog("No keys to display")
+    else:
+      sublime.message_dialog("No Window found")
+
+  def create_quick_panel_item(self, window: sublime.Window, key_info: KeyInfo) -> sublime.QuickPanelItem:
+    trigger: str = key_info.command.value
+    details: str = key_info.key_combo()
+    annotation: str = ""
+    kind = sublime.KIND_AMBIGUOUS
+    quick_panel_item = sublime.QuickPanelItem(trigger, details, annotation, kind)
+    return quick_panel_item
+
+  def when_key_selected(self, index: int) -> None:
+    pass
